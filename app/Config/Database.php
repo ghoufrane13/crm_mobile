b0cdd05e19a14eb6883546035f4fd032f534140e
Ghoufrane <ghoufrane@example.com>
2026-05-18 15:25:15 +0100

    fix: load DB config from Render env variables

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -9,33 +9,22 @@ use CodeIgniter\Database\Config;
  */
 class Database extends Config
 {
-    /**
-     * The directory that holds the Migrations
-     * and Seeds directories.
-     */
     public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
-
-    /**
-     * Lets you choose which connection group to
-     * use if no other is specified.
-     */
     public string $defaultGroup = 'default';
 
     /**
-     * The default database connection.
-     *
      * @var array<string, mixed>
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => '',
         'username'     => '',
         'password'     => '',
         'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
-        'DBDebug'      => true,
+        'DBDebug'      => false,
         'charset'      => 'utf8',
         'DBCollat'     => 'utf8_general_ci',
         'swapPre'      => '',
@@ -49,9 +38,6 @@ class Database extends Config
     ];
 
     /**
-     * This database connection is used when
-     * running PHPUnit database tests.
-     *
      * @var array<string, mixed>
      */
     public array $tests = [
@@ -61,7 +47,7 @@ class Database extends Config
         'password'    => '',
         'database'    => ':memory:',
         'DBDriver'    => 'SQLite3',
-        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
+        'DBPrefix'    => 'db_',
         'pConnect'    => false,
         'DBDebug'     => true,
         'charset'     => 'utf8',
@@ -80,9 +66,24 @@ class Database extends Config
     {
         parent::__construct();
 
-        // Ensure that we always set the database group to 'tests' if
-        // we are currently running an automated test suite, so that
-        // we don't overwrite live data on accident.
+        // Charger les variables d'environnement Render
+        $hostname = getenv('DB_HOSTNAME');
+        if ($hostname) {
+            $this->default['hostname'] = $hostname;
+            $this->default['username'] = getenv('DB_USERNAME') ?: '';
+            $this->default['password'] = getenv('DB_PASSWORD') ?: '';
+            $this->default['database'] = getenv('DB_DATABASE') ?: '';
+            $this->default['port']     = (int)(getenv('DB_PORT') ?: 3306);
+        } else {
+            // Local XAMPP
+            $this->default['hostname'] = 'localhost';
+            $this->default['username'] = 'root';
+            $this->default['password'] = '';
+            $this->default['database'] = 'ton_db_local';
+            $this->default['port']     = 3306;
+            $this->default['encrypt']  = false;
+        }
+
         if (ENVIRONMENT === 'testing') {
             $this->defaultGroup = 'tests';
         }
