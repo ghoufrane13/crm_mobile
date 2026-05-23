5f622397d45db15adc7fa8c5a94bf38cdede9402
Ghoufrane <ghoufrane@example.com>
2026-05-24 00:25:39 +0100

    dernieres changements

--- app/Controllers/PaymentController.php
+++ app/Controllers/PaymentController.php
@@ -496,12 +496,6 @@ class PaymentController extends ResourceController
      */
     public function createPaymeePayment()
     {
-         return $this->respond([
-        'debug_SERVER' => $_SERVER['PAYMEE_API_KEY'] ?? 'SERVER_VIDE',
-        'debug_ENV'    => $_ENV['PAYMEE_API_KEY']    ?? 'ENV_VIDE',
-        'debug_getenv' => getenv('PAYMEE_API_KEY')   ?: 'GETENV_VIDE',
-        'debug_keys'   => array_keys($_ENV),
-    ]);
         $data      = $this->request->getJSON(true);
         $invoiceId = (int)($data['invoice_id'] ?? 0);
         $clientId  = (int)($data['client_id']  ?? 0);
